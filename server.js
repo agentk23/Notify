cf32b43f2699bb37cec1f26364e04c83d1805224
agentk23 <dogaru.costin.169@gmail.com>
2023-01-05 07:38:28 +0200

    init project

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+  
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
